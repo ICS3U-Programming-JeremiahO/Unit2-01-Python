c9b37842daf1f926a87292014e621d4f16e9934e
Jeremiah Omoike <jeremiah.omoike@stu.ocsb.ca>
2022-09-30 19:45:13 +0000

    added comments and fixed style

--- area_per_circle.py
+++ area_per_circle.py
@@ -1,9 +1,23 @@
+#!/usr/bin/env python3
+# Created By: Jeremiah omoike
+# Date: September 30, 2022
+# This program calculates the Area and perimeter of a circle
 import math
 
+def main():
+    print("This program calculates the Area and perimeter of a circle for a radius of 5mm.")
+    radius = 5
 
-import math
+    # calculate the circumference (2*pi*r) and area of a circle(pi*r^2)
+    circumference = math.pi * 2 * radius
+    area = math.pi * radius**2
 
-rad = 5
-circ = math.pi * 2 * rad
-area = math.pi * rad**2
-print(circ, area)
+    # Display the area and circumference of the circle
+    print()
+    print("The area is: {}mm^2".format(area))
+    print("The Circumference is {}mm".format(circumference))
+
+if __name__ == "__main__":
+    main()
+
+
